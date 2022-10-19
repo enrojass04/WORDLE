2a4d97fb3a3444087e6f41e97c52d66e0bfc7d1b
Josefina-Arias <79290474+Josefina-Arias@users.noreply.github.com>
2022-10-18 22:02:51 -0500

    Add files via upload
    Actualización clase gameController

--- src/co/edu/poli/wordle/controller/GameController.java
+++ src/co/edu/poli/wordle/controller/GameController.java
@@ -1,18 +1,28 @@
 package co.edu.poli.wordle.controller;
 
+import java.sql.Connection;
 import java.util.function.UnaryOperator;
 
+
+import co.edu.poli.wordle.conector.ConnectionProyect;
+import co.edu.poli.wordle.conector.ConectionCRUD;
+import co.edu.poli.wordle.model.Word;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class GameController {
-	
-	
-	
+
+	@FXML
+	private AnchorPane anchorPane;
+
+
+
 	private Stage stage;
 
 	private PrincipalController principalController;
@@ -165,7 +175,7 @@ public class GameController {
 	private Button btndelete;
 
 	@FXML
-	private Button btn—;
+	private Button btn√±;
 
 	@FXML
 	private Button btnL;
@@ -193,197 +203,242 @@ public class GameController {
 
 	@FXML
 	private Button btnsend;
-	
-	boolean del = false;
-	
-	 @FXML
-	    void Click_A(ActionEvent event) {
-		 	viewletter("A");
-	    }
 
-	    @FXML
-	    void Click_B(ActionEvent event) {
-	    	viewletter("B");
-	    }
-
-	    @FXML
-	    void Click_C(ActionEvent event) {
-	    	viewletter("C");
-	    }
-
-	    @FXML
-	    void Click_D(ActionEvent event) {
-	    	viewletter("D");
-	    }
-
-	    @FXML
-	    void Click_E(ActionEvent event) {
-	    	viewletter("E");
-	    }
-
-	    @FXML
-	    void Click_F(ActionEvent event) {
-	    	viewletter("F");
-	    }
-
-	    @FXML
-	    void Click_G(ActionEvent event) {
-	    	viewletter("G");
-
-	    }
-
-	    @FXML
-	    void Click_H(ActionEvent event) {
-	    	viewletter("H");
-	    }
-
-	    @FXML
-	    void Click_I(ActionEvent event) {
-	    	viewletter("I");
-	    }
-
-	    @FXML
-	    void Click_J(ActionEvent event) {
-	    	viewletter("J");
-	    }
-
-	    @FXML
-	    void Click_K(ActionEvent event) {
-	    	viewletter("K");
-	    }
-
-	    @FXML
-	    void Click_L(ActionEvent event) {
-	    	viewletter("L");
-	    }
-
-	    @FXML
-	    void Click_M(ActionEvent event) {
-	    	viewletter("M");
-	    }
-
-	    @FXML
-	    void Click_N(ActionEvent event) {
-	    	viewletter("N");
-	    }
-
-	    @FXML
-	    void Click_O(ActionEvent event) {
-	    	viewletter("O");
-	    }
-
-	    @FXML
-	    void Click_P(ActionEvent event) {
-	    	viewletter("P");
-	    }
-
-	    @FXML
-	    void Click_Q(ActionEvent event) {
-	    	viewletter("Q");
-	    }
-
-	    @FXML
-	    void Click_R(ActionEvent event) {
-	    	viewletter("R");
-	    }
-
-	    @FXML
-	    void Click_S(ActionEvent event) {
-	    	viewletter("S");
-	    }
-
-	    @FXML
-	    void Click_T(ActionEvent event) {
-	    	viewletter("T");
-	    }
-
-	    @FXML
-	    void Click_U(ActionEvent event) {
-	    	viewletter("U");
-	    }
-
-	    @FXML
-	    void Click_V(ActionEvent event) {
-	    	viewletter("V");
-	    }
-
-	    @FXML
-	    void Click_W(ActionEvent event) {
-	    	viewletter("W");
-	    }
-
-	    @FXML
-	    void Click_X(ActionEvent event) {
-	    	viewletter("X");
-	    }
-
-	    @FXML
-	    void Click_Y(ActionEvent event) {
-	    	viewletter("Y");
-	    }
-
-	    @FXML
-	    void Click_Z(ActionEvent event) {
-	    	viewletter("Z");
-	    }
-
-	    @FXML
-	    void Click_—(ActionEvent event) {
-	    	viewletter("—");
-	    }
-
-		private int count =0;
-		
-		
-		
-		public String viewletter(String letter) {
-
-			TextField[] lista2 = {txtC00, txtC01, txtC02, txtC03, txtC04};
-
-					if (count<=4) {
-						lista2[count].setText(letter);
-						count++;
-					}
-			
-			return letter;
-		}
-		
-	
-		@FXML
-		void delete(ActionEvent event) {
-			
-		TextField[] lista2 = {txtC00, txtC01, txtC02, txtC03, txtC04};
-				
-			if (count>0) {
-				lista2[count-1].setText("");
-				count--;
-			}	
-		
-		}
-		
-		@FXML
-		void showPrincipal(ActionEvent event) {
-
-			principalController.showGame();
-			stage.close();
-
-		}
-
-		public void init(Stage stage, PrincipalController principalController) {
-
-			this.principalController = principalController;
-			this.stage = stage;
-		}
-
-		@FXML
-		void LimitCharacter(KeyEvent event) {
-
-		}
-		
 	@FXML
-	void btnsend(ActionEvent event) {
+	public TextArea insuwords;
+	
+	@FXML
+	public TextArea nolist;
+
+
+	private ConectionCRUD consultas = new ConectionCRUD();
+
+
+
+	@FXML
+	void Click_A(ActionEvent event) {
+		viewletter("A");
+	}
+
+	@FXML
+	void Click_B(ActionEvent event) {
+		viewletter("B");
+	}
+
+	@FXML
+	void Click_C(ActionEvent event) {
+		viewletter("C");
+	}
+
+	@FXML
+	void Click_D(ActionEvent event) {
+		viewletter("D");
+	}
+
+	@FXML
+	void Click_E(ActionEvent event) {
+		viewletter("E");
+	}
+
+	@FXML
+	void Click_F(ActionEvent event) {
+		viewletter("F");
+	}
+
+	@FXML
+	void Click_G(ActionEvent event) {
+		viewletter("G");
 
 	}
+
+	@FXML
+	void Click_H(ActionEvent event) {
+		viewletter("H");
+	}
+
+	@FXML
+	void Click_I(ActionEvent event) {
+		viewletter("I");
+	}
+
+	@FXML
+	void Click_J(ActionEvent event) {
+		viewletter("J");
+	}
+
+	@FXML
+	void Click_K(ActionEvent event) {
+		viewletter("K");
+	}
+
+	@FXML
+	void Click_L(ActionEvent event) {
+		viewletter("L");
+	}
+
+	@FXML
+	void Click_M(ActionEvent event) {
+		viewletter("M");
+	}
+
+	@FXML
+	void Click_N(ActionEvent event) {
+		viewletter("N");
+	}
+
+	@FXML
+	void Click_O(ActionEvent event) {
+		viewletter("O");
+	}
+
+	@FXML
+	void Click_P(ActionEvent event) {
+		viewletter("P");
+	}
+
+	@FXML
+	void Click_Q(ActionEvent event) {
+		viewletter("Q");
+	}
+
+	@FXML
+	void Click_R(ActionEvent event) {
+		viewletter("R");
+	}
+
+	@FXML
+	void Click_S(ActionEvent event) {
+		viewletter("S");
+	}
+
+	@FXML
+	void Click_T(ActionEvent event) {
+		viewletter("T");
+	}
+
+	@FXML
+	void Click_U(ActionEvent event) {
+		viewletter("U");
+	}
+
+	@FXML
+	void Click_V(ActionEvent event) {
+		viewletter("V");
+	}
+
+	@FXML
+	void Click_W(ActionEvent event) {
+		viewletter("W");
+	}
+
+	@FXML
+	void Click_X(ActionEvent event) {
+		viewletter("X");
+	}
+
+	@FXML
+	void Click_Y(ActionEvent event) {
+		viewletter("Y");
+	}
+
+	@FXML
+	void Click_Z(ActionEvent event) {
+		viewletter("Z");
+	}
+
+	@FXML
+	void Click_√ë(ActionEvent event) {
+		viewletter("√ë");
+	}
+	
+
+
+	@FXML
+	void showPrincipal(ActionEvent event) {
+
+		principalController.showGame();
+		stage.close();
+
+	}
+
+	public void init(Stage stage, PrincipalController principalController) {
+
+		this.principalController = principalController;
+		this.stage = stage;
+	}
+
+
+
+
+	private int count =0;
+	private int intent=0;
+	
+	public TextField[][] matriz() {
+		
+		 TextField[][] lista1 = {{txtC00, txtC01, txtC02, txtC03, txtC04},
+					{txtC10, txtC11, txtC12, txtC13, txtC14},
+					{txtC20, txtC21, txtC22, txtC23, txtC24},
+					{txtC30, txtC31, txtC32, txtC33, txtC34},
+					{txtC40, txtC41, txtC42, txtC43, txtC44},
+					{txtC50, txtC51, txtC52, txtC53, txtC54}};
+		 
+		 return lista1;
+	}
+	
+	public String viewletter(String letter) {
+
+		TextField[][] letters = matriz();
+
+		if (count<=4) {
+			letters[intent][count].setText(letter);
+			count++;
+		}
+
+		return letter;
+	}
+
+
+	@FXML
+	void delete(ActionEvent event) {
+
+		TextField[][] letters = matriz();
+
+		if (count>0) {
+			letters[intent][count-1].setText("");
+			count--;
+		}	
+	}
+
+	@FXML
+	void send(ActionEvent event) {
+		
+		TextField[][] letters = matriz();
+		
+		String letter1 = letters[intent][0].getText();
+		String letter2 = letters[intent][1].getText();
+		String letter3 = letters[intent][2].getText();
+		String letter4 = letters[intent][3].getText();
+		String letter5 = letters[intent][4].getText();
+
+		String wordle = letter1+letter2+letter3+letter4+letter5;
+		Word word  = new Word(1, wordle);
+
+		System.out.println("Word: " + wordle);
+
+		word.checkWord(word, nolist);
+
+		word.insufficientWords(word, insuwords);		
+		
+		if (wordle.length()==5 && word.skipLine(word)) {
+			intent ++;
+			count = 0;
+		}
+	
+
+
+	}
+	
+
 
 
 }
